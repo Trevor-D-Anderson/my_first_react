46c2a10472f14e18b0eacc151c41b8b5f3a806c3
Trevor Anderson <tbone101@gmail.com>
2021-12-01 22:41:29 -0800

    Added Tailwind

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
+import "./App.css";
+import "tailwindcss/tailwind.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Dojo!</h1>
-        <h3>Things I need to do:</h3>
+        <h1 className="text-red-500 text-center">Hello Dojo!</h1>
+        <h3 className="text-blue-600 text-center">Things I need to do:</h3>
         <p>* Learn React</p>
         <p>* Climb Mt Everest</p>
         <p>* Run a marathon</p>
         <p>* Feed the dogs</p>
       </header>
-    </div> 
+    </div>
   );
 }
 
